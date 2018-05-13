827c17a84b3a83701099d7590e41900049e1505f
Olegzandr VD <alex@oresoftware.com>
2018-05-12 21:35:31 -0700

    pdev:'set'

--- postinstall.sh
+++ postinstall.sh
@@ -2,6 +2,11 @@
 
 set -e;
 
+if [[ "$waldo_skip_postinstall" == "yes" ]]; then
+   echo "waldo is skipping postinstall routine.";
+   exit 0;
+fi
+
 waldo_gray='\033[1;30m'
 waldo_magenta='\033[1;35m'
 waldo_cyan='\033[1;36m'
@@ -25,6 +30,16 @@ mkdir -p "$HOME/.waldo/nodejs/waldo"
 #cat run.waldo.sh > "$waldo_bin" && chmod u+x "$waldo_bin" || {
 #  echo "Could not write file or change permissions for file '$waldo_bin'"
 #}
+
+if [[ -z "$(which waldo)" ]]; then
+    waldo_skip_postinstall=yes npm install -g waldo
+fi
+
+#npm_root="$(npm root -g)";
+#
+#ln -sf "$PWD" "$npm_root/waldo" || echo "waldo folder already exists globally"
+#ln -sf "$(npm bin -g)/waldo" "$npm_root/waldo/dist/cli.js" || echo "waldo already installed globally";
+
 
 echo -e "${waldo_green}Waldo was installed successfully.${waldo_no_color}";
 echo -e "Add the following line to your .bashrc/.bash_profile files:";
--- run.waldo.sh
+++ run.waldo.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 
-if [ -f "$HOME/.waldo/nodejs/waldo/dist/cli.js" ]; then
-      node "$HOME/.waldo/nodejs/waldo/dist/cli.js"
+if [[ -n "$(command -v waldo)" ]]; then
+
+  command waldo "$@"
+
 else
-      echo "no waldo available."
-#      npm install -g waldo
-#      waldo "$@"
+
+  "$HOME/.oresoftware/nodejs/node_modules/waldo/index.sh"
+
 fi
 
